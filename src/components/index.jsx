3766c658b06383a5cf804610d8f71bb330f34497
Justin V <justinv4994@gmail.com>
2023-11-01 15:50:28 -0400

    updated Home page; completed HeroBanner component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,14 @@
+import Exercises from './Exercises.jsx'
 import Footer from './Footer.jsx'
 import Navbar from './Navbar.jsx'
+import SearchExercises from './SearchExercises.jsx'
+import HeroBanner from './HeroBanner.jsx'
 
 
 export {
     Footer,
-    Navbar
+    Navbar,
+    HeroBanner,
+    SearchExercises,
+    Exercises
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import { Box } from '@mui/material';
+
+
+// importing components
+import { HeroBanner, SearchExercises, Exercises } from '../components';
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+      </Box>
     </>
-  )
+  );
 };
 
 export default Home;
